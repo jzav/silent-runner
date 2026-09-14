5d9f38c6a1e2ea83d989ae62bc3c82486b498777
jzav <32063115+jzav@users.noreply.github.com>
2026-09-14 16:24:34 +0200

    - initial public release

--- src/HelpGenerator.cpp
+++ src/HelpGenerator.cpp
@@ -6,7 +6,7 @@ namespace HelpGenerator {
 
 std::wstring Generate() {
     std::wstring err =
-        L"Usage:\n"
+        L"Usage of v1.0.0:\n"
         L"  SilentRunner.exe [options] <script-or-exe> [args...]\n"
         L"      Execute script or executable via cmd.exe /d /s /c.\n"
         L"      The file must be executable in the user's Windows environment.\n"
